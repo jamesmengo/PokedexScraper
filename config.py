03362247e2048c2adadcebc9a401003018517b38
James <jmeng@coveo.com>
2020-10-13 14:46:52 -0700

    Cleanup

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-sourceId = None
-orgId = None
-apiKey = None
+sourceId = "jmengpokemoncr4dcczm-tbxxiq7ebhmzxv5zvtrlytknae"
+orgId = "jmengpokemoncr4dcczm"
+apiKey = "xxdb990d89-968f-4639-952d-bb1722255421"
